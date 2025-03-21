877d29fc2695ed5b904449b1245def55c4534304
AmrittBhusal <138292976+AmrittBhusal@users.noreply.github.com>
2025-03-21 16:04:02 +0000

    add variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,12 @@
+const accountname = "Amrit"
+let accountemail = "amrit.nt.7@gmail.com"
+var accountpswd = 1233
+accountaddress = "Dang"
+
+// accountname = "bhusal"
+
+accountemail = "amrit.nt@gmail.com"
+accountpswd = 345
+accountaddress  = "kathmandu"
+
+console.table([accountname,accountemail,accountpswd,accountaddress]);
